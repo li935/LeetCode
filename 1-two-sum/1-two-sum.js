46c05e26d464af9c8cf7be0026ba7038038361a0
Donghyun Kim <bandits0726@gmail.com>
2022-08-25 06:06:00 +0900

    Time: 115 ms (58.78%), Space: 43.1 MB (32.00%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -7,13 +7,12 @@ var twoSum = function(nums, target) {
     const result = new Map();
     
     for (let i = 0; i < nums.length; i++) {
-        const firstNum = nums[i];
         const secondNum = target - nums[i];
         
         if (result.has(secondNum)) {
             return [result.get(secondNum), i];
         }
         
-        result.set(firstNum, i);
+        result.set(nums[i], i);
     }
 };
