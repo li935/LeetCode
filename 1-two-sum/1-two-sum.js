803433168c173d3bba49a2120178f24711fdae7b
Donghyun Kim <bandits0726@gmail.com>
2022-08-25 05:39:52 +0900

    Time: 301 ms (8.45%), Space: 42.6 MB (53.96%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        const one = target - nums[i];
+        
+        for (let j = 0; j < nums.length; j++) {
+            if (i !== j) {
+                if (one - nums[j] === 0) {
+                    return [i, j];
+                }
+            }
+        }
+    }
+};
