8fa6188f2d1caddea26984cd8dd813edcacb24fe
Donghyun Kim <bandits0726@gmail.com>
2022-08-28 01:31:59 +0900

    Time: 131 ms (9.29%), Space: 42.7 MB (45.88%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -5,20 +5,19 @@
 var longestCommonPrefix = function(strs) {
     let prefix = "";
     
-    strs.sort((a, b) => a.length - b.length);
+    // strs.sort((a, b) => a.length - b.length);
     
     const shortestWord = strs[0].length;
-    let tempPrefix = "";
     
     for (let i = 0; i < shortestWord; i++) {
-        tempPrefix += strs[0][i];
+        prefix += strs[0][i];
         
         for (let j = 1; j < strs.length; j++) {
-            if (tempPrefix[i] !== strs[j][i]) {
-                return tempPrefix.slice(0, -1);
+            if (prefix[i] !== strs[j][i]) {
+                return prefix.slice(0, -1);
             }
         }
     }
     
-    return tempPrefix;
+    return prefix;
 };
