7d3155665849de8966460ca3101a7bf3d6a496ae
Donghyun Kim <bandits0726@gmail.com>
2022-08-30 00:56:18 +0900

    Time: 629 ms (5.01%), Space: 54.2 MB (5.14%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    const test = (str) => {
+        const strIdx = [];
+        const isomorphic = [];
+        
+        str.split("").map(c => {
+            const idx = strIdx.indexOf(c);
+            
+            if (idx < 0) {
+                strIdx.push(c);
+                isomorphic.push(strIdx.indexOf(c));
+            } else {
+                isomorphic.push(idx);
+            }
+        })
+        
+        return isomorphic;
+    };
+    
+    const test2 = (str1, str2) => {
+        for (let i = 0; i < str1.length; i++) {
+            console.log(str1[i] + ", " + str2[i]);
+            if (str1[i] !== str2[i]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    console.log(test(s));
+    console.log(test(t));
+    
+    return test2(test(s), test(t));
+    
+
+};
