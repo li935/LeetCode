d8f5bb37a5bc34ce0fc81b0e233cf2fe6ea86735
Donghyun Kim <bandits0726@gmail.com>
2022-09-01 23:41:13 +0900

    Time: 97 ms (40.85%), Space: 42 MB (89.06%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -2,9 +2,7 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function(nums) {
-    if (nums.length === 1) return nums[0];
-        
+var runningSum = function(nums) {  
     for (let i = 1; i < nums.length; i++) {
         nums[i] += nums[i - 1];
     }
