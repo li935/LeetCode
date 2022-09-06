9edd2c672c6bac5daebe803040e4392d6d6b5a03
Donghyun Kim <bandits0726@gmail.com>
2022-09-07 04:50:24 +0900

    Time: 96 ms (37.06%), Space: 41.8 MB (73.21%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -3,7 +3,5 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {    
-    let result = (BigInt(digits.join("")) + 1n).toString();
-    
-    return result.split("").map(e => e);
+    return (BigInt(digits.join("")) + 1n).toString().split("");
 };
