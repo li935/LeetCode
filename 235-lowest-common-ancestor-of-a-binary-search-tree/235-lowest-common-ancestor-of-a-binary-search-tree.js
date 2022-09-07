7ff749fee29e1db52f666fbf7d7e19c001c53553
Donghyun Kim <bandits0726@gmail.com>
2022-09-08 04:08:43 +0900

    Time: 101 ms (80.20%), Space: 51.5 MB (96.91%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,79 +13,11 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    let parentNode = root;
-    root.parent = null;
-    
-    let rootLevel = 1;
-    
-    let pNodeChecker = false;
-    let qNodeChecker = false;
-    
-    while (root) {
-        root.visited = true;
-        root.level = rootLevel;
-        
-        if (root.val === p.val) {
-            p = root;
-            pNodeChecker = true;
-        }
-        
-        if (root.val == q.val) {
-            q = root;
-            qNodeChecker = true;
-        }
-        
-        // console.log(root.val, root.level);
-        
-        if (root.left === null && root.right === null) {
-            root = root.parent;
-            rootLevel--;
-        } else if (root.left !== null) {
-            if (root.left.visited === undefined) {
-                root.left.parent = root;
-                root = root.left;
-                rootLevel++;
-            } else if (root.right !== null) {
-                if (root.right.visited === undefined) {
-                    root.right.parent = root;
-                    root = root.right;
-                    rootLevel++;
-                } else {
-                    root = root.parent;
-                    rootLevel--;
-                }
-            } else {
-                root = root.parent;
-                rootLevel--;
-            }
-        } else {
-           if (root.right !== null) {
-                if (root.right.visited === undefined) {
-                    root.right.parent = root;
-                    root = root.right;
-                    rootLevel++;
-                } else {
-                    root = root.parent;
-                    rootLevel--;
-                }
-            } else {
-                root = root.parent;
-                rootLevel--;
-            } 
-        }
+    if (root.val > p.val && root.val > q.val) {
+        return lowestCommonAncestor(root.left, p, q);
+    } else if (root.val < p.val && root.val < q.val) {
+        return lowestCommonAncestor(root.right, p, q);
     }
     
-    while (true) {
-        if (p.val === q.val) {
-            break;
-        } else {
-            if (p.level > q.level && p.level > 1) {
-                p = p.parent;
-            } else {
-                q = q.parent;
-            }
-        }
-    }
-    
-    return p;
+    return root;
 };
