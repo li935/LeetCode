67bd0fec98b71fee6aea8528460255ec691ca9ad
Donghyun Kim <bandits0726@gmail.com>
2022-09-08 04:10:27 +0900

    Time: 81 ms (97.31%), Space: 51.9 MB (81.09%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,6 +13,10 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
+    if (root.val === p.val || root.val === q.val) {
+        return root;
+    }
+    
     if (root.val > p.val && root.val > q.val) {
         return lowestCommonAncestor(root.left, p, q);
     } else if (root.val < p.val && root.val < q.val) {
