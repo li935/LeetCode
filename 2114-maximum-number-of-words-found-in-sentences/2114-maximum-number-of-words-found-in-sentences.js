11f41fd8cbc12d89a5475f75dc3c69c92523637f
Donghyun Kim <bandits0726@gmail.com>
2022-09-17 01:08:06 +0900

    Time: 75 ms (85.25%), Space: 43.2 MB (91.85%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -3,7 +3,5 @@
  * @return {number}
  */
 var mostWordsFound = function(sentences) {
-    return Math.max(...sentences.map(sentence => {
-        return sentence.split("").filter(e => e === " ").length;
-    })) + 1;
+    return Math.max(...sentences.map(s => s.split(" ").length));
 };
