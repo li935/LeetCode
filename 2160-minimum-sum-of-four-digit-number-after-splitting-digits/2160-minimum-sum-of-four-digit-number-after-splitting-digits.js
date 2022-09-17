aa147e845d4dd65abef24a6d8876fac06191f572
Donghyun Kim <bandits0726@gmail.com>
2022-09-18 04:42:47 +0900

    Time: 119 ms (5.62%), Space: 42.9 MB (6.04%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
@@ -7,26 +7,18 @@ var minimumSum = function(num) {
     
     for (let i = 0; i < 4; i++) {
         for (let j = i + 1; j < 4; j++) {
-            const copyNum = num.toString().split("");
+            const numB = num.toString().split("");
+            const numA = numB[i] + numB[j];
             
-            const numA = copyNum[i] + copyNum[j];
-            
-            const a = Number.parseInt(copyNum.splice(i, 1) + copyNum.splice(j - 1, 1));
-            const b = Number.parseInt(Array.from(copyNum).join(""));
+            const a = Number.parseInt(numB.splice(i, 1) + numB.splice(j - 1, 1));
+            const b = Number.parseInt(Array.from(numB).join(""));
             const c = Number.parseInt(Array.from(numA).reverse().join(""));
-            const d = Number.parseInt(Array.from(copyNum).reverse().join(""));
+            const d = Number.parseInt(Array.from(numB).reverse().join(""));
             
-            console.log(a, b, c, d);
+            const target = Math.min(a + b, a + d, c + b, c + d);
             
-            const A = a + b;
-            const B = a + d;
-            const C = c + b;
-            const D = c + d;
-            
-            const X = Math.min(a + b, a + d, c + b, c + d);
-            
-            if (minNum > X) {
-                minNum = X;
+            if (minNum > target) {
+                minNum = target;
             }
         }
     }
